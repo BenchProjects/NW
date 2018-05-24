279adedce906e98a7a9f6b11b15c4068950bebef
Bill Hire <c1983c@hotmail.com>
2018-05-24 10:11:36 +0000

    Seperating code into functions

--- AWS_DeployToECR.ps1
+++ AWS_DeployToECR.ps1
@@ -1,40 +1,57 @@
 ﻿param([Int32]$tag=0, $image="")
 
+function login-To-ecr(){
+    $loginStatement = aws ecr get-login --no-include-email --region us-east-1 
+    $loginStatementArray = $loginStatement -split ' '
+    $username = $loginStatementArray[3]
+    $password = $loginStatementArray[5]
+    $ecr_endpoint = $loginStatementArray[6]
+
+    echo "$password" | docker login -u $username --password-stdin $ecr_endpoint
+
+    if (-not $?)
+    {
+        throw "Login failed, please check AWS credentials are set up on this host"
+    } 
+}
+
+function verify-parameters(){
+    if ($tag -eq 0)
+    {
+        throw "Parameter exception, tag was not passed"
+    }
+
+    if ($image -eq "")
+    {
+        throw "Parameter exception, container image name was not passed"
+    }
+
+    Write-Output "Image name: $image"
+    Write-Output "Tag: $tag"
+}
+
+function push-to-ecr(){
+    docker tag 825688464055.dkr.ecr.us-east-1.amazonaws.com/benchprojects/nw:$tag 825688464055.dkr.ecr.us-east-1.amazonaws.com/pws/container_repo:latest
+
+    Write-Output "Source image successfully tagged"
+
+    docker push 825688464055.dkr.ecr.us-east-1.amazonaws.com/pws/container_repo:latest
+}
 
 Write-Output "Script version: 1"
 
-if ($tag -eq 0)
-{
-    throw "Parameter exception, tag was not passed"
-}
+#1 Verify script parameters
+Write-Output "Attempting to verify script parameters"
+verify-parameters
 
-if ($image -eq "")
-{
-    throw "Parameter exception, container image name was not passed"
-}
-
-Write-Output "Image name: $image"
-Write-Output "Tag: $tag"
-
+#2 login in to AWS
 Write-Output "Attempting to log in to AWS"
+login-To-ecr
 
-$loginStatement = aws ecr get-login --no-include-email --region us-east-1 
-$loginStatementArray = $loginStatement -split ' '
-$username = $loginStatementArray[3]
-$password = $loginStatementArray[5]
-$ecr_endpoint = $loginStatementArray[6]
+#3 push container image to AWS
+Write-Output "Attempting to push to AWS"
+push-to-ecr
 
-echo "$password" | docker login -u $username --password-stdin $ecr_endpoint
-
-if (-not $?)
-{
-    throw "Login failed, please check AWS credentials are set up on this host"
-} 
-
-
-docker tag 825688464055.dkr.ecr.us-east-1.amazonaws.com/benchprojects/nw:$tag 825688464055.dkr.ecr.us-east-1.amazonaws.com/pws/container_repo:latest
-
-docker push 825688464055.dkr.ecr.us-east-1.amazonaws.com/pws/container_repo:latest
 
 if ($?){
     "Script completed successfully"
