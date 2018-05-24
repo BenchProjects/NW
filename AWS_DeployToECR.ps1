d44a2d9b2cd36782ea5fa50153aab543d456b20e
Bill Hire <c1983c@hotmail.com>
2018-05-24 11:12:32 +0000

    Removing hardcoded values

--- AWS_DeployToECR.ps1
+++ AWS_DeployToECR.ps1
@@ -1,4 +1,13 @@
-﻿param([Int32]$tag=0, $image="")
+﻿param([Int32]$tag=0, $image="", $awsRepositoryName = "")
+
+#Example call
+# AWS_DeployToECR.ps1 38 123.dkr.ecr.us-east-1.amazonaws.com/myproject/nw mycontainerrepo
+
+#Prerequisites:
+# Machine must have Docker and AWS tools installed.
+# Your container image is already built on your machine.
+# Your AWS credentials have already been set on this machine.
+# You have an AWS ECR repository.
 
 function login-To-ecr(){
     $loginStatement = aws ecr get-login --no-include-email --region us-east-1 
@@ -7,12 +16,17 @@ function login-To-ecr(){
     $password = $loginStatementArray[5]
     $ecr_endpoint = $loginStatementArray[6]
 
+    $imagePrefix = $ecr_endpoint -replace "https://", ""
+    $tempRepositoryImageName = $imagePrefix + "/" + $awsRepositoryName + ":latest"
+
     echo "$password" | docker login -u $username --password-stdin $ecr_endpoint
 
     if (-not $?)
     {
         throw "Login failed, please check AWS credentials are set up on this host"
     } 
+    
+    return $tempRepositoryImageName
 }
 
 function verify-parameters(){
@@ -26,16 +40,30 @@ function verify-parameters(){
         throw "Parameter exception, container image name was not passed"
     }
 
+    if ($awsRepositoryName -eq "")
+    {
+        throw "Parameter exception, repository name was not passed"
+    }
+
+    Write-Output "Repository name: $awsRepositoryName"
     Write-Output "Image name: $image"
     Write-Output "Tag: $tag"
 }
 
-function push-to-ecr(){
-    docker tag 825688464055.dkr.ecr.us-east-1.amazonaws.com/benchprojects/nw:$tag 825688464055.dkr.ecr.us-east-1.amazonaws.com/pws/container_repo:latest
+function push-to-ecr([string]$finalRepositoryImageName = ""){
+
+    if ($finalRepositoryImageName -eq "")
+    {
+        throw "Unable to determine aws repository to push to"
+    }
+
+    $taggedImageReference = $image + ":" + $tag
+
+    docker tag $taggedImageReference $finalRepositoryImageName
 
     Write-Output "Source image successfully tagged"
 
-    docker push 825688464055.dkr.ecr.us-east-1.amazonaws.com/pws/container_repo:latest
+    docker push $finalRepositoryImageName
 }
 
 Write-Output "Script version: 1"
@@ -46,11 +74,12 @@ verify-parameters
 
 #2 login in to AWS
 Write-Output "Attempting to log in to AWS"
-login-To-ecr
+$loginOutput = login-To-ecr
+$finalRepositoryImageName =$loginOutput[1]
 
 #3 push container image to AWS
 Write-Output "Attempting to push to AWS"
-push-to-ecr
+push-to-ecr $finalRepositoryImageName
 
 
 if ($?){
